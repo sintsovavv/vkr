22d30afe0c38b97c838c05760b8d6668b1878280
sintsovavv <sintsovavv@gmail.com>
2023-04-02 15:13:03 +0300

    updated app.py

--- vkr_app_flask/app.py
+++ vkr_app_flask/app.py
@@ -2,13 +2,17 @@ import flask
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
+import pandas as pd
 import sklearn
 from sklearn.linear_model import LinearRegression
+from sklearn import preprocessing
+from sklearn.preprocessing import MinMaxScaler
 
 app = flask.Flask(__name__, template_folder = 'templates')
 
 model_1 = pickle.load(open('model_for_mod.pkl', 'rb'))
 model_2 = pickle.load(open('model_for_str.pkl', 'rb'))
+preprocessor = pickle.load(open('preprocessor.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -18,9 +22,11 @@ def home():
 def predict():
     int_features = [float(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
-    prediction = model_1.predict(final_features)
+    scaler = MinMaxScaler()
+    final_features_pred = scaler.fit_transform(final_features)
+    prediction = model_1.predict(final_features_pred)
     output_1 = round(prediction[0], 2)
-    prediction_2 = model_2.predict(final_features)
+    prediction_2 = model_2.predict(final_features_pred)
     output_2 = round(prediction_2[0], 2) 
     return render_template('mail.html', output1 = output_1, output2 = output_2)
 
